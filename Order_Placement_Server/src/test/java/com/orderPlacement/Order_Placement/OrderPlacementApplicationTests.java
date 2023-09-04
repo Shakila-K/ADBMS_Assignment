d411f22738d79535e8a0bbce523a0b19bb204b3a
Shakila K Bandara <skbandara32@gmail.com>
2023-09-04 13:01:14 +0530

    initial commit
    User management, Inventory management and order placement servers

---
+++ Order_Placement_Server/src/test/java/com/orderPlacement/Order_Placement/OrderPlacementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderPlacement.Order_Placement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPlacementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
